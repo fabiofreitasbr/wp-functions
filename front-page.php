b468d7319e25338a2952c35a5a0be305eb4d2b60
Fábio Freitas <fabinho25682568@gmail.com>
2018-10-16 17:25:59 -0300

    -a

--- front-page.php
+++ front-page.php
@@ -76,6 +76,7 @@ get_header();
 		</div>
 	</footer>
 </div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 <script src="../../assets/js/vendor/popper.min.js"></script>
@@ -88,6 +89,7 @@ get_header();
 		text: 'Thumbnail'
 	});
 </script>
+
 <?php
 get_footer();
 ?>
